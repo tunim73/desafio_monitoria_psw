c98729f2f05887633fb7db126b76174f39d57757
tunim73 <antonio.netto973@gmail.com>
2024-02-18 18:18:38 -0300

    add bootstrap. Início da Página Home.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router.jsx";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,67 @@
+const listCelular = [
+  {
+    id: 1,
+    marca: "Samsung",
+    modelo: "Galaxy S21",
+    capacidade_memoria_gb: 256,
+    data_lancamento: "2021-01-29",
+  },
+  {
+    id: 2,
+    marca: "Xiaomi",
+    modelo: "Redmi Note 10",
+    capacidade_memoria_gb: 128,
+    data_lancamento: "2021-03-04",
+  },
+  {
+    id: 3,
+    marca: "Motorola",
+    modelo: "Moto G Power",
+    capacidade_memoria_gb: 64,
+    data_lancamento: "2020-04-01",
+  },
+];
+
 export const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="container mt-5">
+      <button type="button" className="btn btn-success mb-3">
+        Novo Celular
+      </button>
+      <table className="table text-center table-bordered">
+        <thead>
+          <tr>
+            <th>Marca</th>
+            <th>Modelo</th>
+            <th>Capacidade de memória (GB)</th>
+            <th>Data de lançamento</th>
+            <th>Alterar</th>
+            <th>Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listCelular.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>{item.marca}</td>
+                <td>{item.modelo}</td>
+                <td>{item.capacidade_memoria_gb}</td>
+                <td>{item.data_lancamento}</td>
+                <td>
+                  <button type="button" className="btn btn-warning">
+                    Alterar
+                  </button>
+                </td>
+                <td>
+                  <button type="button" className="btn btn-danger">
+                    Excluir
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
