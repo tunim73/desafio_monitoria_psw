0920880f9c6bedd78ce468416f91ffca686e73aa
tunim73 <antonio.netto973@gmail.com>
2024-02-20 15:45:27 -0300

    config cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import { connectDatabase } from "./database/index.js";
 import { celularController } from "./controllers/celular.controller.js";
 
@@ -8,7 +9,7 @@ await connectDatabase();
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json(), cors());
 
 app
   .get("/", celularController.test)
@@ -17,7 +18,7 @@ app
   .get("/celulares", celularController.findAll)
   .put("/celular/:id", celularController.update)
   .delete("/celular/:id", celularController.destroy);
-  
+
 app.listen(PORT, () => {
   console.log(`Running on PORT ${PORT}`);
 });
