5c8aea8653cb04bc316d6f376cc9acc4825481e0
tunim73 <antonio.netto973@gmail.com>
2024-02-20 19:45:17 -0300

    update: Update integração com api

--- frontend/src/pages/Update.jsx
+++ frontend/src/pages/Update.jsx
@@ -1,18 +1,46 @@
+import { useNavigate, useParams } from "react-router-dom";
 import { Form } from "../components";
+import { useEffect, useState } from "react";
 
 export const Update = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const [celular, setCelular] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/celular/${id}`)
+      .then(async (res) => {
+        const data = await res.json();
+
+        setCelular(data.Celular);
+      })
+      .catch((error) => {
+        console.error("err: ", error);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const updateSubmit = async (data) => {
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    try {
+      await fetch(`http://localhost:3000/celular/${id}`, options);
+      navigate(`/update/${id}`);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  };
+
   return (
     <>
-      <Form
-        tipo={"update"}
-        values={{
-          id: 1,
-          marca: "Samsung",
-          modelo: "Galaxy S21",
-          capacidade_memoria_gb: 256,
-          data_lancamento: "2021-01-29",
-        }}
-      />
+      <Form type={"update"} values={celular} aoSubmit={updateSubmit} />
     </>
   );
 };
