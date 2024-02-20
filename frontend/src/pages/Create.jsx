50ec91606bfdb014c2e97cebbefda838fd6515f4
tunim73 <antonio.netto973@gmail.com>
2024-02-20 19:44:55 -0300

    update: Create integração com api

--- frontend/src/pages/Create.jsx
+++ frontend/src/pages/Create.jsx
@@ -1,5 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import { Form } from "../components";
 
 export const Create = () => {
-  return <Form tipo={"create"} values={null} />;
+  const navigate = useNavigate();
+
+  const createSubmit = async (data) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    try {
+      await fetch("http://localhost:3000/celular", options);
+      navigate("/");
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  };
+
+  return <Form type={"create"} values={null} aoSubmit={createSubmit} />;
 };
