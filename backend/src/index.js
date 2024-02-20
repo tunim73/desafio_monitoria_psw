ea1615c95b6659094e973e4582b3f6486af1e186
tunim73 <antonio.netto973@gmail.com>
2024-02-20 14:31:17 -0300

    add  body-parser no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,20 @@
 import express from "express";
-import { connectDatabase } from "./database";
+import bodyParser from "body-parser";
+import { connectDatabase } from "./database/index.js";
+
+await connectDatabase();
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Running on PORT ${PORT}`);
-});
-
-await connectDatabase();
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+  res.status(200).json({
+    msg: "helloWorld",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on PORT ${PORT}`);
 });
