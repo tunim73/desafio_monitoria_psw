b78d9f486b8221899187aed06cea9de63e91d3b9
tunim73 <antonio.netto973@gmail.com>
2024-02-25 00:05:23 -0300

    fix: remove import react toastfy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
